1e5cd775d2cdf678122da2adf993745942c571b7
Mischa Untaga <99098079+MischaU8@users.noreply.github.com>
2025-03-04 20:27:44 +0100

    remove unused

--- src/data/stations.ts
+++ src/data/stations.ts
@@ -157,35 +157,4 @@ export const DefaultStationConfigs: StationConfig[] = [
             }
         }
     }
-]
-export enum Stations {
-    refinery = 'refinery',
-    shipyard = 'shipyard',
-    repair = 'repair',
-    trading = 'trading',
-}
-
-export const StationTypes = {
-    [Stations.refinery]: {
-        name: 'Refinery',
-        image: Resources.StationA,
-        color: ex.Color.Brown
-    },
-    [Stations.shipyard]: {
-        name: 'Shipyard',
-        image: Resources.StationB,
-        color: ex.Color.Blue
-    },
-    [Stations.repair]: {
-        name: 'Repair',
-        image: Resources.StationC,
-        color: ex.Color.Green
-    },
-    [Stations.trading]: {
-        name: 'Trading',
-        image: Resources.StationA,
-        color: ex.Color.Red
-    }
-} as const;
-
-export type StationTypeKey = keyof typeof StationTypes;
+] as const;

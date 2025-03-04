d93457512e0627ea02f079079b984987db3a4a15
Mischa Untaga <99098079+MischaU8@users.noreply.github.com>
2025-03-04 20:08:44 +0100

    set prices

--- src/components/station.ts
+++ src/components/station.ts
@@ -5,7 +5,7 @@ import { Wares, WaresType } from '../data/wares';
 
 export class StationComponent extends ex.Component {
     declare owner: ex.Actor
-    itemPrices: { [key in WaresType]: number };
+    itemPrices: { [key in WaresType]: number }
 
     private random: ex.Random;
 
@@ -13,19 +13,20 @@ export class StationComponent extends ex.Component {
         super();
         this.random = random;
         this.itemPrices = {} as { [key in WaresType]: number };
-        this.initializeBasePrices();
     }
 
     initializeBasePrices() {
-        // Initialize with default values for all wares
-        this.itemPrices = Object.fromEntries(
-            Object.keys(Wares).map(key => [key, 0])
-        ) as { [key in WaresType]: number };
-
-        // Set random prices for all wares
+        // Set random prices for all wares in our cargo filter
+        const resourceFilter = this.owner.get(CargoComponent)?.resourceFilter;
         for (const [key, value] of Object.entries(Wares)) {
-            this.itemPrices[key as WaresType] = this.random.integer(value.minPrice, value.maxPrice);
+            if (resourceFilter && resourceFilter.includes(key as WaresType)) {
+                this.itemPrices[key as WaresType] = this.random.integer(value.minPrice, value.maxPrice);
+            }
         }
+    }
+
+    onAdd(owner: ex.Actor): void {
+        this.initializeBasePrices();
     }
 
     getDetails(): string {
@@ -41,10 +42,8 @@ wallet ${this.owner.get(WalletComponent)?.getDetails()}
     getCargoDetailsWithPrices(): string {
         const cargo = this.owner.get(CargoComponent);
         let details = `${cargo.volume.toFixed(0)}/${cargo.maxVolume.toFixed(0)}m³`;
-        if (cargo.volume > 0) {
-            const items = Object.entries(cargo.items).map(([item, amount]) => `${item.padStart(8, " ")} ${amount.toFixed(0)}x (¢${this.itemPrices[item as WaresType].toFixed(0)})`);
-            details += `\n${items.join('\n')}`;
-        }
+        const prices = Object.entries(this.itemPrices).map(([item, price]) => `${item.padStart(8, " ")} ${cargo.items[item as WaresType] || 0}x ¢${price.toFixed(0)}`);
+        details += `\n${prices.join('\n')}`;
         return details;
     }
 }
